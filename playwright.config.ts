bef0aa3a0176189894456e637fa3977a55a7151a
merr1412 <khlueabwichit@gmail.com>
2026-04-22 00:25:57 +0700

    chore: update webServer configuration to use cwd and pipe output streams

--- playwright.config.ts
+++ playwright.config.ts
@@ -72,8 +72,11 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'npm run dev --prefix client',
+    command: 'npm run dev',
+    cwd: './client',
     url: 'http://localhost:3000',
     reuseExistingServer: !process.env.CI,
+    stdout: 'pipe',
+    stderr: 'pipe',
   },
 });
